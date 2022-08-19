984874485e152ed9a7574a276eac3b2867650f11
Jaibw <jai.unix@gmail.com>
2022-08-19 10:11:31 +0530

    Update send.py

--- send.py
+++ send.py
@@ -1,10 +1,13 @@
 import pika
+from datetime import datetime 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+message = str(datetime.now()) + ' Hello World!'
+
+channel.basic_publish(exchange='', routing_key='hello', body=message)
+print(" [x] Sent ",message)
 connection.close()
